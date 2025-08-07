fffedd281b5cee1459b025ceb1896b9cd8b405a1
Nicolas Peralta <nperaltapz@gmail.com>
2025-08-06 21:00:08 -0400

    restructuration of the agents and its methods done, the main change was to split all the assets characteristics in different maps and each one is an attribute of the agent. Assets are now bundles

--- agent/agent.go
+++ agent/agent.go
@@ -1,40 +1,48 @@
 package agent
 
-type assetFeature map[string]float32
+type bundleFeature map[string]float32
 
 type agent struct {
-	assets *[]string
-	assets_amount assetFeature
-	assets_participation assetFeature
-	assets_MRS assetFeature
-	assets_price_min assetFeature
-	assets_price_max assetFeature
+	budget float32
+	price_rule float32
+	bundle *[]string
+	bundle_amount bundleFeature
+	bundle_participation bundleFeature
+	bundle_MRS bundleFeature
+	bundle_price_min bundleFeature
+	bundle_price_max bundleFeature
+	bundle_price_average bundleFeature
+	bundle_optimal bundleFeature
 }
 
-func NewAgent(assets *[]string) agent {
+func NewAgent(bundles *[]string) agent {
 
-	var asset_features_zeroes map[string]float32 
+	var bundle_features_zeroes map[string]float32 
 	
-	for _, name := range *assets {
-		asset_features_zeroes[name] = 0
+	for _, name := range *bundles {
+		bundle_features_zeroes[name] = 0
 	}
 
 	a := agent{
-		assets: assets,
-		assets_amount: asset_features_zeroes,
-		assets_participation: asset_features_zeroes,
-		assets_MRS: asset_features_zeroes,
-		assets_price_min: asset_features_zeroes,
-		assets_price_max: asset_features_zeroes,
+		budget: 0,
+		price_rule: 0.5,
+		bundle: bundles,
+		bundle_amount: bundle_features_zeroes,
+		bundle_participation: bundle_features_zeroes,
+		bundle_MRS: bundle_features_zeroes,
+		bundle_price_min: bundle_features_zeroes,
+		bundle_price_max: bundle_features_zeroes,
+		
 	}
 
 	a.randomParticipation()
 	a.randomAmounts()
 	a.mrsCalculator()
-
-	a.assets_price_min = a.assets_MRS
-	a.assets_price_max = a.assets_MRS
-	
+	a.bundle_price_min = a.bundle_MRS
+	a.bundle_price_max = a.bundle_MRS
+	a.bundle_price_average = a.bundle_MRS
+	a.calculateBudget()
+	a.calculateOptimalAmount()
 
 	return a
 }
@@ -49,7 +57,7 @@ func NewAgent(assets *[]string) agent {
 // and then is going to be updated for the last price he or she payed in
 // grand exchange. But also could be a set of prices, max and min or something like that
 
-// The assets inventory must also contain price for each good, 
+// The bundles inventory must also contain price for each good, 
 // There should be a method for calculate the RMS between money and other goods,
 // The good that is going to serve as money should be clarified,
 
--- agent/utils.go
+++ agent/utils.go
@@ -7,7 +7,7 @@ import (
 
 func (a agent) randomParticipation() {
     total := 100
-	parts := len(*a.assets)
+	parts := len(*a.bundle)
 
     cuts := make([]int, parts-1)
     for i := 0; i < parts-1; i++ {
@@ -24,24 +24,47 @@ func (a agent) randomParticipation() {
     }
 
 	counter := 0
-	for name, _ := range a.assets_participation {
-		a.assets_participation[name] = result[counter]
+	for name, _ := range a.bundle_participation {
+		a.bundle_participation[name] = result[counter]
 		counter += 1
 	}
 
 }
 
 func (a agent) randomAmounts() {
-	for name, _ := range a.assets_amount {
-		a.assets_amount[name] = float32(rand.Float64()*99 + 1)
+	for name, _ := range a.bundle_amount {
+		a.bundle_amount[name] = float32(rand.Float64()*99 + 1)
 	}
 }
 
 func (a agent) mrsCalculator() {
-	money_name := (*a.assets)[0]
-	for name, _ := range a.assets_MRS {
-		quotient_amount := a.assets_amount[money_name] / a.assets_amount[name]
-		quotient_participation := a.assets_participation[name] / a.assets_participation[money_name]
-		a.assets_MRS[name] = quotient_participation * quotient_amount
+	money_name := (*a.bundle)[0]
+	for name, _ := range a.bundle_MRS {
+		quotient_amount := a.bundle_amount[money_name] / a.bundle_amount[name]
+		quotient_participation := a.bundle_participation[name] / a.bundle_participation[money_name]
+		a.bundle_MRS[name] = quotient_participation * quotient_amount
+	}
+}
+
+func (a agent) calculateAveragePrice() {
+	for name, _ := range a.bundle_price_max {
+		price := (a.bundle_price_max[name] * a.price_rule) + (a.bundle_price_min[name] * a.price_rule)
+		a.bundle_price_average[name] = price 
+	}
+}
+
+func (a agent) calculateBudget() {
+	var budget float32 
+	for name, _ := range a.bundle_price_max {
+		budget += a.bundle_price_average[name] * a.bundle_amount[name]
+	}
+
+	a.budget = budget	         
+}
+
+func (a agent) calculateOptimalAmount() {
+	for name, _ := range a.bundle_optimal {
+		bundle_optimal_money := a.budget * a.bundle_participation[name]
+		a.bundle_optimal[name] = bundle_optimal_money / a.bundle_price_average[name]
 	}
 }
